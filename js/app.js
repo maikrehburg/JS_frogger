83358df070a10ac75a295fa3c0227865c4f7f96b
maikrehburg <33413818+maikrehburg@users.noreply.github.com>
2019-11-29 20:52:21 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,159 @@
+// Enemies our player must avoid
+const maxVerticalLaneIdx = 5;
+const maxHorizontalLaneIdx = 4;
+const yZeroPos = -20;
+const xZeroPos = 0;
+
+const yLaneShift = 85;
+var yPosForLane = [];
+for (var i = 0; i <= maxVerticalLaneIdx; i++) {
+    yPosForLane[i] = yZeroPos + (i * yLaneShift);
+}
+
+const xLaneShift = 100;
+var xPosForLane = [];
+for (var i = 0; i <= maxHorizontalLaneIdx; i++) {
+    xPosForLane[i] = xZeroPos + (i * xLaneShift);
+}
+
+const bugMovingLine = {start: -101, finish: 501};
+
+
+var Enemy = function() {
+    this.x = bugMovingLine.start;
+    this.y = yPosForLane[Math.floor(Math.random() * 3) + 1];
+    this.speed = (Math.random() * 100 + 50);
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/enemy-bug.png';
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    this.x += this.speed * dt;
+    
+    // Let bug start on new random lane if end of gamescreen is reached
+    if (this.x > bugMovingLine.finish) {
+        this.x = bugMovingLine.start;
+        this.y = yPosForLane[Math.floor(Math.random() * 3) + 1];
+    }
+    
+    this.checkCollision(player);
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Enemy.prototype.checkCollision = function(player) {
+    if (this.y === player.y) {
+        if ((this.x > player.x - 75) &&
+           (this.x < player.x + 75)) {
+            alert('Game over!');
+            player.reset();
+        }
+    }
+}
+
+
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+var Player = function() {
+    this.x;// = xPosForLane[playerStartPoint.x];
+    this.y;// = yPosForLane[playerStartPoint.y];
+    this.sprite = 'images/char-boy.png';
+    
+    this.reset();
+};
+
+Player.prototype.update = function() {
+    switch (this.ctrlKey) {
+        case 'up' :
+            this.y -= yLaneShift;
+            break;
+        case 'down' :
+            if (this.y < yPosForLane[maxVerticalLaneIdx]) {
+                this.y += yLaneShift;
+            }
+            break;
+        case 'left' :
+            if (this.x > xPosForLane[0]) {
+                this.x -= xLaneShift;
+            }
+            break;
+        case 'right' :
+            if (this.x < xPosForLane[maxHorizontalLaneIdx]) {
+                this.x += xLaneShift;
+            }
+            break;
+        default:
+            break;
+    }
+    this.ctrlKey = null;
+    
+    if (this.y === yPosForLane[0]) {
+        alert('You won!');
+        this.reset();
+    }
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(key) {
+    this.ctrlKey = key;
+}
+
+Player.prototype.reset = function() {
+    const playerStartPoint = {x: 2, y: 5};
+
+    this.x = xPosForLane[playerStartPoint.x];
+    this.y = yPosForLane[playerStartPoint.y];
+}
+
+
+/*
+var Item = function() {
+    ;
+}
+
+Item.prototype.render(dt) 
+Item.prototype.appear = function() {
+    ;
+}
+*/
+
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+var allEnemies = [];
+var numberOfEnemies = 4;
+
+for (var i = 0; i < numberOfEnemies; i++) {
+    allEnemies.push(new Enemy());
+}
+
+var player = new Player();
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
+
